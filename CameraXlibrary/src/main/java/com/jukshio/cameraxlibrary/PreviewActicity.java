03ebf2f7eade5fd65379c26669397c01e42ae3d9
NagaVenkatesh <nagavenkatesh.paritala.jukshio.com>
2022-05-05 12:41:04 +0530

    preview into view

--- CameraXlibrary/src/main/java/com/jukshio/cameraxlibrary/PreviewActicity.java
+++ CameraXlibrary/src/main/java/com/jukshio/cameraxlibrary/PreviewActicity.java
@@ -1,288 +1,165 @@
 package com.jukshio.cameraxlibrary;
 
-import android.content.ContentResolver;
-import android.content.ContentValues;
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Matrix;
-import android.hardware.camera2.CameraAccessException;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.hardware.camera2.CameraManager;
-import android.net.Uri;
 import android.os.Build;
-import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.util.AttributeSet;
-import android.util.Log;
-import android.util.Rational;
-import android.util.Size;
-import android.view.Surface;
+import android.view.TextureView;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.CameraX;
+import androidx.camera.core.AspectRatio;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.ImageCapture;
-import androidx.camera.core.ImageCaptureConfig;
 import androidx.camera.core.Preview;
-import androidx.camera.core.PreviewConfig;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 
+import com.google.common.util.concurrent.ListenableFuture;
+
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.Objects;
+import java.util.concurrent.ExecutionException;
 
 public class PreviewActicity extends View {
 
-    AutoFitTextureView textureView;
-    private int REQUEST_CODE_PERMISSIONS = 101;
-    private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE"};
-    public static final int MY_PERMISSIONS_REQUEST_CAMERA = 100;
-    public static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1;
-    PreviewConfig pConfig;
-    Preview preview;
+    Context mContext;
+    PreviewView previewView;
+    private final int REQUEST_CODE = 1001;
+
+    Button button;
     Bitmap bitmap;
-    OutputStream outputStream;
-    ImageCaptureConfig imageCapture;
-    ImageCapture imgCap;
-    Uri imageuri;
-    ContentResolver resolver;
-    ContentValues contentValues;
+    static Context context;
+    TextView textView;
+    static int width1, height1;
+    File file;
+    ImageCapture imageCapture;
+    ImageCapture.OutputFileOptions outputFileOptions;
+    private final String[] Requires_Permission = new String[]{"android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE"};
     private CameraManager cameraManager;
     private String getCameraID;
     private boolean flashLight = true;
-    Context context;
-
-
+    CameraSelector cameraSelector;
+    ImageAnalysis imageAnalysis;
+    Preview preview;
+    ProcessCameraProvider cameraProvider;
 
     public PreviewActicity(Context context) {
         super(context);
-        this.context = context;
-        textureView = new AutoFitTextureView(context);
+
 
     }
 
     public PreviewActicity(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        mContext = context;
     }
 
     public PreviewActicity(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context, attrs, defStyleAttr);
     }
 
-    public PreviewActicity(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
+    public void init(Context context, AttributeSet attributeSet, int defStyleAttr) {
+        mContext = context;
+        Toast.makeText(context, "Context", Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
-        if (allPermissionsGranted()) {
-            startCamera();
+        Toast.makeText(mContext, "hi", Toast.LENGTH_SHORT).show();
+        if (allpermissionGranted()) {
+            getPreviewView();
         } else {
-            Toast.makeText(context, "Permissions not granted by the user", Toast.LENGTH_SHORT).show();
+            ActivityCompat.requestPermissions((Activity) mContext, Requires_Permission, REQUEST_CODE);
         }
+
 
     }
 
-    /*
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        textureView = new AutoFitTextureView(this);
-        setContentView(textureView);
 
-        if (allPermissionsGranted()) {
-            startCamera(); //start camera if permission has been granted by user
-
-        } else {
-            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
-        }
-        capturePhotos();
-        //flashLightOn();
-    }
-*/
-
-    public void startCamera() {
-
-        CameraX.unbindAll();
-
-        Rational aspectRatio = new Rational(textureView.getWidth(), textureView.getHeight());
-        Size screen = new Size(textureView.getWidth(), textureView.getHeight()); //size of the screen
-
-
-        pConfig = new PreviewConfig.Builder().setTargetAspectRatio(aspectRatio).setTargetResolution(screen).build();
-        preview = new Preview(pConfig);
-
-        preview.setOnPreviewOutputUpdateListener(
-                new Preview.OnPreviewOutputUpdateListener() {
-                    //to update the surface texture we  have to destroy it first then re-add it
-                    @Override
-                    public void onUpdated(Preview.PreviewOutput output) {
-                        ViewGroup parent = (ViewGroup) textureView.getParent();
-                        parent.removeView(textureView);
-                        parent.addView(textureView, 0);
-
-                        textureView.setSurfaceTexture(output.getSurfaceTexture());
-                        updateTransform();
-                    }
-                });
-/*
-
-        ImageCaptureConfig imageCaptureConfig = new ImageCaptureConfig.Builder().setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
-                .setTargetRotation(getWindowManager().getDefaultDisplay().getRotation()).build();
-        final ImageCapture imgCap = new ImageCapture(imageCaptureConfig);
-
-        File file = new File(Environment.getExternalStorageDirectory() + "/" + System.currentTimeMillis() + ".png");
-        imgCap.takePicture(file, new ImageCapture.OnImageSavedListener() {
+    public PreviewView getPreviewView() {
+        ListenableFuture<ProcessCameraProvider> cameraProviderListenableFuture = ProcessCameraProvider.getInstance(mContext);
+        cameraProviderListenableFuture.addListener(new Runnable() {
             @Override
-            public void onImageSaved(@NonNull File file) {
-                String msg = "Pic captured at " + file.getAbsolutePath();
-                Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
-            }
+            public void run() {
+                try {
+                    cameraProvider = cameraProviderListenableFuture.get();
 
-            @Override
-            public void onError(@NonNull ImageCapture.UseCaseError useCaseError, @NonNull String message, @Nullable Throwable cause) {
-                String msg = "Pic capture failed : " + message;
-                Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
-                if (cause != null) {
-                    cause.printStackTrace();
+                    preview = new Preview.Builder().setTargetAspectRatio(AspectRatio.RATIO_4_3).build();
+                    cameraSelector = new CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
+                    imageAnalysis = new ImageAnalysis.Builder().build();
+                    imageCapture = new ImageCapture.Builder().build();
+                    preview.setSurfaceProvider(previewView.getSurfaceProvider());
+                    cameraProvider.bindToLifecycle((LifecycleOwner) mContext, cameraSelector, imageAnalysis, imageCapture, preview);
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
-        });*/
+        }, ContextCompat.getMainExecutor(mContext));
 
+        return previewView;
     }
 
-    public void capturePhotos() {
 
-
-/*
-        File direct = new File(Environment.getExternalStorageDirectory() + "/DirName");
-
-        if (!direct.exists()) {
-            File wallpaperDirectory = new File("/sdcard/DirName/");
-            wallpaperDirectory.mkdirs();
-        }
-
-        File file = new File("/sdcard/DirName/", System.currentTimeMillis() + ".jpg");
-        if (file.exists()) {
-            file.delete();
-        }
-        try {
-            FileOutputStream out = new FileOutputStream(file);
-            Bitmap original = BitmapFactory.decodeStream(getAssets().open("500x250.jpg"));
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            original.compress(Bitmap.CompressFormat.PNG, 100, out);
-
-            byteArrayOutputStream.flush();
-            byteArrayOutputStream.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-*/
-/*
-        try {
-            imageCapture = new ImageCaptureConfig.Builder().setCaptureMode(ImageCapture.CaptureMode.MAX_QUALITY)
-                    .setTargetRotation(context.getWindowManager().getDefaultDisplay().getRotation()).build();
-            imgCap = new ImageCapture(imageCapture);
-            resolver = context.getContentResolver();
-            contentValues = new ContentValues();
-            contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, "Image_" + ".jpg");
-            contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
-            contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES + File.separator + "CameraXFolder");
-            imageuri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
-            outputStream = (FileOutputStream) resolver.openOutputStream(Objects.requireNonNull(imageuri));
-            File file = new File(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES + File.separator + "CameraXFolder");
-            Objects.requireNonNull(outputStream);
-            imgCap.takePicture(file, new ImageCapture.OnImageSavedListener() {
-                @Override
-                public void onImageSaved(@NonNull File file) {
-                    Toast.makeText(context, "Photo Saved" + file, Toast.LENGTH_SHORT).show();
-                    Log.e("TAG", "onImageSaved: " + file);
-                }
-
-                @Override
-                public void onError(@NonNull ImageCapture.UseCaseError useCaseError, @NonNull String message, @Nullable Throwable cause) {
-
-                    Toast.makeText(context, "error", Toast.LENGTH_SHORT).show();
-                }
-            });
-
-            CameraX.bindToLifecycle((LifecycleOwner) this, preview, imgCap);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-*/
-
-    }
-
-    public void updateTransform() {
-        Matrix mx = new Matrix();
-        float w = textureView.getMeasuredWidth();
-        float h = textureView.getMeasuredHeight();
-
-        float cX = w / 2f;
-        float cY = h / 2f;
-
-        int rotationDgr;
-        int rotation = (int) textureView.getRotation();
-
-        switch (rotation) {
-            case Surface.ROTATION_0:
-                rotationDgr = 0;
-                break;
-            case Surface.ROTATION_90:
-                rotationDgr = 90;
-                break;
-            case Surface.ROTATION_180:
-                rotationDgr = 180;
-                break;
-            case Surface.ROTATION_270:
-                rotationDgr = 270;
-                break;
-            default:
-                return;
-        }
-
-        mx.postRotate((float) rotationDgr, cX, cY);
-        textureView.setTransform(mx);
-    }
-
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
-                startCamera();
+    public boolean allpermissionGranted() {
+        for (String permission : Requires_Permission) {
+            if (ContextCompat.checkSelfPermission(mContext, permission) == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(mContext, "Permission Granted", Toast.LENGTH_SHORT).show();
+                return true;
             } else {
-                Toast.makeText(context, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show();
-
+                Toast.makeText(mContext, "Permission Denied", Toast.LENGTH_SHORT).show();
             }
+        }
+        return false;
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void bindlifecycle(ProcessCameraProvider cameraProvider) {
+        cameraProvider.bindToLifecycle((LifecycleOwner) mContext, cameraSelector, imageAnalysis, imageCapture, preview);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            //  imagecaptureofcamerax();
         }
     }
 
-    public boolean allPermissionsGranted() {
+/*
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    private void imagecaptureofcamerax() {
 
-        for (String permission : REQUIRED_PERMISSIONS) {
-            if (ContextCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
-                return false;
+        imageCapture.takePicture(outputFileOptions, mContext.getMainExecutor(), new ImageCapture.OnImageSavedCallback() {
+            @Override
+            public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                Toast.makeText(mContext, "Photo Saved", Toast.LENGTH_SHORT).show();
+
             }
-        }
-        return true;
+
+            @Override
+            public void onError(@NonNull ImageCaptureException exception) {
+
+                Toast.makeText(mContext, "Error" + exception.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
+*/
 
 }
